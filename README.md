75a1eac53fc030306bdfd78e121439f3a9a0b4f1
Thinh Tran <thinhtran@outlook.com.au>
2021-09-15 20:35:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Configure Github
+
+From `Repository/Settings/Pages` select `gh-pages` branch, with `/root` folder
+
+
+[mauiautomation.github.io](mauiautomation.github.io)
+[mauiautomation.com](mauiautomation.com)
+
+## Deploy
+`yarn deploy`
+
+This command will push all static content to `gh-pages` branch
---
+++ src/pages/404.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="404: Not Found" />
+      <main className="error-page">
+        <h1>404</h1>
+        <h3>Page Not Found</h3>
+        <br />
+        <p>What are the first 403 errors?</p>
+      </main>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
